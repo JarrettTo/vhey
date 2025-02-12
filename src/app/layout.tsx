44a30a725194b09d4a1b12fb4a99ecdb82261bc8
jkrperson <josekristianresabal@gmail.com>
2025-02-12 21:03:35 +0800

    feat[site]: Add analytics for the site.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import "../styles/prism-vsc-dark-plus.css";
 import ToasterContext from "./api/contex/ToasetContex";
 import { useEffect, useState } from "react";
 import PreLoader from "@/components/Common/PreLoader";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({
   children,
@@ -48,6 +49,7 @@ export default function RootLayout({
             </ThemeProvider>
           </SessionProvider>
         )}
+        <Analytics />
       </body>
     </html>
   );
