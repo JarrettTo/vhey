a9120382e59a88d03a4a43f3588fb6da3a96f2f5
JarrettTo <justin.jarrett.to@gmail.com>
2025-02-13 22:16:29 +0800

    Agents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,6 +108,7 @@ export default function Home() {
         <Benefits/>
         <Tasks />
         <AIProcess/>
+        <Personal/>
         <StaffingProfiles/>
         <CallToAction />
       </div>
--- src/components/Personal/index.tsx
+++ src/components/Personal/index.tsx
@@ -21,30 +21,30 @@ const Personal = () => {
           backgroundRepeat: "no-repeat", // Prevents the background from repeating
         }}
       >
-        <h1 className=" text-4xl xl:text-5xl text-center text-primary font-light">
-          Not work related?<br/>
-          <span className="font-bold">Not a problem.</span>
+        <h1 className=" text-4xl xl:text-4xl text-center text-primary font-light">
+          End-to-end<br/>
+          <span className="font-medium">Talent Management</span>
         </h1>
-        <p className="text-md xl:text-xl text-primary text-center mt-4 font-light">
-          Nothing's too personal for your expert.
+        <p className="text-md xl:text-xl text-primary text-center mt-4 font-light flex flex-row items-center gap-2">
+          with <span><img src="/images/logo.svg" className="h-8" height={10} width={"auto"}></img></span> AI
         </p>
       </div>
-      <div className="block lg:hidden w-4/4 px-5 md:px-0 md:w-3/4 h-full flex flex-col items-center justify-center"
+      <div className="block lg:hidden w-4/4 px-5 md:px-0 md:w-3/4 h-full w-full flex flex-col items-center justify-center bg-[#101518]"
       >
       <Image
         className="block lg:hidden mb-12"
-        src="/images/mobilepersonal.png"
+        src="/images/personal/mobile.svg"
         alt="Agent Icon"
         width={200} // Adjust width
         height={200} // Adjust height
       />
-      <h1 className="text-4xl text-center text-primary font-light md:text-5xl">
-        Not work related?<br/>
-        <span className="font-bold">Not a problem.</span>
-      </h1>
-      <p className="text-lg text-primary text-center mt-4 font-light">
-        Ask them to book flights, order food, schedule appointments, etc.
-      </p>
+      <h1 className=" text-3xl  text-center text-primary font-light">
+          End-to-end<br/>
+          <span className="font-medium">Talent Management</span>
+        </h1>
+        <p className="text-md xl:text-xl text-primary text-center mt-4 font-light flex flex-row items-center gap-2">
+          with <span><img src="/images/logo.svg" className="h-8" height={10} width={"auto"}></img></span> AI
+        </p>
       </div>
     </section>
   );
